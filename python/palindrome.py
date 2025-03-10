a5a1e2e9cc9931b46de4790faf60d8fa1aab11f3
robertuptc <robertuptc@gmail.com>
2025-03-10 17:30:53 -0400

    python problem finished

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,18 @@
+import re, math
+
 def palindrome(word):
-    # Write code here
+    clean_word = re.sub(r"[^a-zA-Z0-9]", "", str(word).lower())
+    
+    first_half = ''
+    second_half = ''
+
+    if len(clean_word) % 2 != 0:
+        half = math.floor(len(clean_word)/2)
+        first_half = clean_word[0 : half]
+        second_half = clean_word[half + 1 : ][::-1]
+    else :
+        half = int(len(clean_word) / 2)
+        first_half = clean_word[0 : half]
+        second_half = clean_word[half:][::-1]
+    return True if first_half == second_half  else False
+
--- python/palindrome_spec.py
+++ python/palindrome_spec.py
@@ -2,7 +2,7 @@
 
 from palindrome import palindrome
 # This should return a bunch of trues
-print(palindrome('racecar') == True)
+print(palindrome('race car') == True)
 print(palindrome('Noon') == True)
 print(palindrome('ciVic') == True)
 print(palindrome('nice') == False)
@@ -10,6 +10,6 @@ print(palindrome(434) == True)
 print(palindrome(123) == False)
 print(palindrome('bomb') == False)
 
-print("The following should be True if you're trying to do the extra portion of this challenge")
+# print("The following should be True if you're trying to do the extra portion of this challenge")
 print(palindrome('Sore was I ere I saw Eros.') == True)
 print(palindrome('A man, a plan, a canal -- Panama') == True)
